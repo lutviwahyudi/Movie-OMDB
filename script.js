b6ae57dc2aba9c577e48a015d120c1143990a593
wahyulut <lutvi.190170110@gmail.com>
2023-12-30 23:56:10 +0700

    selesai

--- script.js
+++ script.js
@@ -1,37 +1,76 @@
-$('.search-button').on('click', function () {
-    $.ajax({
-        url: 'http://www.omdbapi.com/?apikey=b2e42ea8&s=' + $('.input-keyword').val(),
-        success: result => {
-            const movie = result.Search
-            let cards = ''
-            movie.forEach(m => {
-                cards += showCards(m)
-            });
-            // panggil cards masukan kedalam html
-            $('.movie').html(cards)
+// $('.search-button').on('click', function () {
+//     $.ajax({
+//         url: 'http://www.omdbapi.com/?apikey=b2e42ea8&s=' + $('.input-keyword').val(),
+//         success: result => {
+//             const movie = result.Search
+//             let cards = ''
+//             movie.forEach(m => {
+//                 cards += showCards(m)
+//             });
+//             // panggil cards masukan kedalam html
+//             $('.movie').html(cards)
     
-            // jika tombol detail di klik
-            $('.modal-detail-button').on('click', function () {
-                $.ajax({
-                    url: 'http://www.omdbapi.com/?apikey=b2e42ea8&i=' + $(this).data('imdbid'),
-                    success: m => {
-                        const detail = showDetail(m)
-                        $('.modal-body').html(detail)
-                    }
+//             // jika tombol detail di klik
+//             $('.modal-detail-button').on('click', function () {
+//                 $.ajax({
+//                     url: 'http://www.omdbapi.com/?apikey=b2e42ea8&i=' + $(this).data('imdbid'),
+//                     success: m => {
+//                         const detail = showDetail(m)
+//                         $('.modal-body').html(detail)
+//                     }
+//                 })
+//             })
+    
+//         },
+//         error: e => {
+//             console.log(e.responseText)
+//         }
+//     })
+// })
+
+// fecth
+const button = document.querySelector('.search-button')
+button.addEventListener('click', function () {
+
+    const inputKeyword = document.querySelector('.input-keyword')
+    fetch('http://www.omdbapi.com/?apikey=b2e42ea8&s=' + inputKeyword.value)
+    .then(response => response.json())
+    .then(response => {
+        const movie = response.Search
+        let cards = ''
+        movie.forEach(m => cards += showCards(m))
+        const movies = document.querySelector('.movie')
+        movies.innerHTML = cards
+
+
+        // jika tombol showDetail diklik
+        const detail = document.querySelectorAll('.modal-detail-button')
+        detail.forEach(btn => {
+            btn.addEventListener('click', function () {
+                const imdbid = this.dataset.imdbid
+                fetch('http://www.omdbapi.com/?apikey=b2e42ea8&i=' + imdbid)
+                .then(response => response.json())
+                .then(m => {
+                    const movieDetail = showDetail(m)
+                    const mDetail = document.querySelector('.modal-body')
+                    mDetail.innerHTML = movieDetail
                 })
             })
-    
-        },
-        error: e => {
-            console.log(e.responseText)
-        }
+        })
     })
 })
 
 
 
+
+
+
+
+
+
+
 function showCards(m) {
-    return `<div class="col-md-3 my-4">
+    return `<div class="col-md-4 my-5">
             <div class="card" style="width: 18rem;">
                 <img src="${m.Poster}" class="card-img-top" alt="">
                 <div class="card-body">
